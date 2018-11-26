25d7d7be23d5d0bb604ba2c7961b79bc57dd8033
Виталик Любимов <us@itstep1.dp.ua>
2018-11-26 13:03:09 +0200

    Change category.cs

--- Feedback/Category.cs
+++ Feedback/Category.cs
@@ -4,6 +4,7 @@ namespace Feedback
 {
     public class Category
     {
+        // Первичный ключ
         public int Id { get; set; }
         public string Name { get; set; }
         public virtual ICollection<Good> Goods { get; set; }
