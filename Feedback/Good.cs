d102d05c2058de02e2a05313da9b5582062a7c8b
Виталик Любимов <us@itstep1.dp.ua>
2018-11-26 12:55:34 +0200

    Add project

---
+++ Feedback/Good.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Feedback
+{
+    public class Good
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        //Обратная связь
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Feedback/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feedback
+{
+    class DatabaseContext : DbContext
+    {
+        static DatabaseContext()
+        {
+            Database.SetInitializer(new DatabaseInitializer());
+        }
+        public DatabaseContext() : base("myConnection")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Good> Goods { get; set; }
+    }
+
+    class DatabaseInitializer : DropCreateDatabaseAlways<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext ctx)
+        {
+            Category category = new Category
+            {
+                Name = "Category 1",
+                Goods = new List<Good>
+                {
+                    new Good { Name = "Good 1", Price = 22.5m },
+                    new Good { Name = "Good 2", Price = 35.7m },
+                }
+            };
+            ctx.Entry(category).State = EntityState.Added;
+            ctx.SaveChanges();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DatabaseContext ctx = new DatabaseContext())
+            {
+                var users = ctx.Categories.ToList();
+                ;
+            }
+            Console.Read();
+        }
+    }
+}
